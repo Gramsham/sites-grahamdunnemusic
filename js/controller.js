76b609cc3499997d13362ddd786c0ae356afeca6
Graham Dunne <grahamdunne@graham-dunnes-macbook-pro.local>
2017-01-23 00:38:40 +0000

    push

--- js/controller.js
+++ js/controller.js
@@ -2,24 +2,5 @@ angular.module('RouteControllers', [])
 
     .controller('HomeController', function($scope) {
         /*$scope.title = "Welcome To The Website of Graham Dunne";*/
-    })  
-
-    .controller('AboutController', function($scope) {
-        /*$scope.title = "Welcome To The Website of Graham Dunne";*/
-    })
-    .controller('ProductionsController', function($scope) {
-        /*$scope.title = "Welcome To The Website of Graham Dunne";*/
-    })
-    .controller('CompositionsController', function($scope) {
-        /*$scope.title = "Welcome To The Website of Graham Dunne";*/
-    })
-    .controller('GuitarController', function($scope) {
-        /*$scope.title = "Welcome To The Website of Graham Dunne";*/
-    })
-    .controller('TranscriptionsController', function($scope) {
-        /*$scope.title = "Welcome To The Website of Graham Dunne";*/
-    })
-    .controller('ContactController', function($scope) {
-        /*$scope.title = "Welcome To The Website of Graham Dunne";*/
     });
     
